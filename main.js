d99be69d70933548df0d921dce86e241e16b85ec
Oughlane <oughlanemohammed72@gmail.com>
2025-07-16 18:15:05 +0200

    edit js and add readme

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-
-//calculator project
-const display = document.getElementById('display'); // l input dial kola display
-
-const buttons = document.querySelectorAll(".calculator_keys button");
-
-
-
-function clearDisplay() {
-    display.value = ''; // cleari screeen 
-}
-let currentInput = '';
+function setupCalculator() {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll(".calculator_keys button");
+let currentInput =  '';
+let firstNumber = null;
+let operator = null
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        button.addEventListener('click', () => {
-           const value = button.textContent; // tatched value ila t clicka
-       
-        });
-    if (value === 'C') {
-        
+        const value = button.textContent; //    tatched value ila t clicka
+
+        if (!isNaN(value) || value === '.') {
+//number o fassila
+            currentInput += value;
+            display.value = currentInput;//ntl3 current input
+        } else if (value === 'C'){
+            //mss7 kolchi
+            currentInput = '';
+            firstNumber = null;
+            operator = null
+display.value = "";  
+     }
+     else if(value === "=") {
+   if (firstNumber !== null && operator null && currentInput ()
+   const secondNumber = parseFloat(currentInput);
+   let result;
+   switch (operator){ 
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        case '/':
+            result = firstNumber / secondNumber;
+            break;
+        default:
+            return; // ila makynch no operator  set
+    }
+    display.value = result; // Show the result in the display
+    currentInput = ''; // Reset current input for next calculation
+    firstNumber = null; // Reset first number for next calculation
+    operator = null; // Reset operator for next calculation 
+   }
