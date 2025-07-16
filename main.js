d32d88914d549971c828733d7ad580ff487397ba
Oughlane <oughlanemohammed72@gmail.com>
2025-07-16 15:35:48 +0200

    add js  principal functions

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 
 //calculator project
-const display = document.getElementById('display');
-function appendToDisplay(input) {
-    display.value += input;
+const display = document.getElementById('display'); // l input dial kola display
+
+const buttons = document.querySelectorAll(".calculator_keys button");
+
+
+
+function clearDisplay() {
+    display.value = ''; // cleari screeen 
 }
+
+let clearDisplay   
+let  dial
+let  value const packageName = require('packageName');
+let operator = '';
+let firstValue = '';
+let secondValue = '';
+let result = '';
+let isOperatorClicked = false;
+let isEqualClicked = false;
+let isDecimalClicked = false;
+let isNegative = false;   
+let isPercentClicked = false;
+scrollTo function scrollToTop() {
+    window.scrollTo(0, 0); // scroll to top of the pag
+}
+let isClear = false;
+length
