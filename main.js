f55883024cd2019d9981ed17585c3b2090f50e58
Oughlane <oughlanemohammed72@gmail.com>
2025-07-17 17:37:02 +0200

    develop the structure of code source by mrAziz

--- main.js
+++ main.js
@@ -1,62 +1,14 @@
-function setupCalculator() {
-    const display = document.getElementById("display");
-    const buttons = document.querySelectorAll(".calculator_keys button");
 
-    let currentInput = ''; // Initialize current input as an empty string
+var displayDiv = document.getElementById("display")
+var buttons = document.querySelectorAll(".buttons button")
 
-    let firstNumber = null;
-    let operator = null;
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const value = button.textContent; //    tatched value ila t clicka
-
-        if (!isNaN(value) || value === ".") {
-//number o fassila
-            currentInput += value;
-            display.value = currentInput;//ntl3 current input
-        } else if (value === "C") {
-            //mss7 kolchi
-            currentInput = "";
-            firstNumber = null;
-            operator = null;
-            display.value = "";  
-     
-        }else if(value === "=") {
-   if (firstNumber !== null && operator !== null  && currentInput !== "") {
-   const secondNumber = parseFloat(currentInput);
-   let result;
-   switch (operator){ 
-        case '+':
-            result = firstNumber + secondNumber;
-            break;
-        case '-':
-            result = firstNumber - secondNumber;
-            break;
-        case '×':
-            result = firstNumber * secondNumber;
-            break;
-        case '/':
-            result = secondNumber !== 0 ? firstNumber / secondNumber : 'Error'; // n handle cases dial div sur 0
-            break;
-        default:
-            return "Error"; // ila makynch no operator  set
+buttons.forEach(btn=>{
+    btn.onclick = function () {
+    if (btn.innerHTML=="=") {
+    displayDiv.innerHTML = eval(displayDiv.innerHTML)
+    }else if (btn.innerHTML=="C"){
+        displayDiv.innerHTML = ""
+    } else
+        displayDiv.innerHTML += btn.innerHTML
     }
-    display.value = result; // Show the result in the display
-    currentInput = result.toString(); // Reset current input for next calculation
-    firstNumber = null; // Reset first number for next calculation
-    operator = null; // Reset operator for next calculation 
-   }
-} else {  
-
-    if (currentInput !== " ") {
-        firstNumber = parseFloat(currentInput);
-        operator = value; // Settti operator
-        currentInput = ""; // resetti current input  l next n
-    }
-}
-    });
-});
-}
-setupCalculator(); // Call the function to set up the calculator
-       
+})
